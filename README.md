e9ca921d70f87385b0ad603cbce9c7c6dba89414
kaiya gunsauls <kaiyagunsauls@gmail.com>
2021-10-02 20:02:40 -0700

    api search working, just need to make 5-day-forecast feature. recent search also needs to become links as well

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # weather-dash
+
+weather dashboard pulls current and forecasted weather data into a simple dashboard format.
+
+side bar contains search bar, submit button, and links to recently searched cities.
+
+data retrieved from OpenWeather API and measurements are in imperial units.
+
+main dashboard includes temperature, wind, humidity, and uv data for current city, data is from the time of search
+
+lower card deck includes temp, wind, and humidity data for the next 5-day-forecast in searched city. 
+
+recent searches are included in local storage and will remain in list even on page reload
+
--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,12 @@ console.log("what's up");
 
 //input vars
 var searchInput = document.getElementById('city-input');
-var cityInput = JSON.stringify(searchInput);
+var cityInput = $("#city-input").val();
 var searchBtn = document.getElementById("search-btn");
-var APIkey = "47523fcfa432220823eeddae011ae353";
+var APIkey = "&appid=47523fcfa432220823eeddae011ae353";
+var api = "http://api.openweathermap.org/data/2.5/weather?q="
 
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + document.getElementById("city-input").value + "&appid=" + APIkey;
+
 //used to check function
 var searches = [];
 var recentList = document.getElementById('recent-search');
@@ -14,25 +15,40 @@ var recentList = document.getElementById('recent-search');
 
 searchBtn.addEventListener("click", function(event) {
     event.preventDefault();
-
-    
-    console.log(queryURL);
-
-    if (inputText === "") {
-      return;
-    }
-
-    
-  
-    searches.push(inputText);
-    searchInput.value = "";
-
-    console.log(cityInput)
+    console.log(searchInput.value)
+    getWeather();
     storeInput();
     renderLastSearch();
-    // getData();
-  });
+});
 
+
+
+function getWeather() {
+
+    var url = api + searchInput.value + "&units=imperial" + APIkey;
+    console.log(url);
+
+    fetch(url)
+    .then(function (response) {
+        if (response.ok) {
+            
+            response.json().then(data => {
+                var nameValue = data['name'];
+                var temp = data['main']['temp'];
+                var wind = data['wind']['speed'];
+                var humid = data['main']['humidity'];
+                var desc = data['weather'][0]['description'];
+
+                document.getElementById('temp').innerHTML = temp + "°F";
+                document.getElementById('current-search-city').innerHTML = nameValue;
+                document.getElementById('wind').innerHTML = wind + "mph";
+                document.getElementById('humid').innerHTML = humid + "%";
+                document.getElementById('desc').innerHTML = desc;
+            });
+        
+        };
+    });
+};
 
 //puts search value into localstorage
 function storeInput() {
@@ -67,7 +83,3 @@ function init() {
 }
 
 init()
-
-// function getData() {
-//     console.log(queryURL);
-// }
